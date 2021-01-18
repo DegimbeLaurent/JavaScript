1b7d7eb993aad2024a50877d0c5bf3e9c1e8e522
Laurent.D <laurent.degimbe@gmail.com>
2021-01-18 12:09:33 +0100

    Control-structures

---
+++ 2.1.js
@@ -0,0 +1,7 @@
+var age = prompt("Quel est ton age");
+
+if(this.age>=18){
+    console.log("Vous êtes un adulte")
+}else{
+    console.log("Vous n'êtes pas un adulte")
+}
---
+++ 2.2.js
@@ -0,0 +1,13 @@
+var n1 = prompt("min ?");
+var n2 = prompt("max ?");
+var n3 = prompt("current ?");
+
+if(this.n1 < this.n2){
+    if(this.n3 <= this.n2 && this.n3 >= this.n1){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+}else{
+    console.log("ta rien compris");
+}
