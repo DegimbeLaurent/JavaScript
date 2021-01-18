75cbf7ee1860919a5c75c1fc644b2bcc711d6a03
Laurent.D <laurent.degimbe@gmail.com>
2021-01-18 14:46:13 +0100

    3.2

---
+++ 3.2.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5];
+var som = new Number(0);
+arr.forEach(element => {
+    som = som+element;
+});
+console.log(som/5);
+
+let arr2 = [100,101,102];
+var som2 = new Number(0);
+arr2.forEach(element => {
+    som2 = som2+element;
+});
+console.log(som2/3);
