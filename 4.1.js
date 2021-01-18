fd21c1109855840693ea96bf8dd53176305a8135
Laurent.D <laurent.degimbe@gmail.com>
2021-01-18 17:10:03 +0100

    4.1 doc

--- 4.1.js
+++ 4.1.js
@@ -1,9 +1,12 @@
 const readline = require("readline-sync");
+
 function calcSurfaceU(){
     var n1 = readline.question("longeur?");
     var n2 = readline.question("largeur?");
     console.log(calcSurface(n1,n2));
 }
+
+/*Multiplie 2 nombre en eux*/ 
 function calcSurface(a,b){
     return a*b;
 }
