d351504a21f1a51550ac1926520d49337742905f
Laurent.D <laurent.degimbe@gmail.com>
2021-01-19 10:23:55 +0100

    4.5

--- 4.5.js
+++ 4.5.js
@@ -1,7 +1,11 @@
 const readline = require("readline-sync");
+
+/* recois 2 corrdonnée et renvoie la distance entre ces 2 points */
 function calcDistance(x1,y1,x2,y2){
     return Math.sqrt((calcPoint(x2,x1)+calcPoint(y2,y1)));
 }
+
+/*recois deux nombre (x || y) et revoie le carré de la division de ces 2 nombres*/ 
 function calcPoint(n1,n2){
     return Math.pow( (n2-n1) ,2);
 }
